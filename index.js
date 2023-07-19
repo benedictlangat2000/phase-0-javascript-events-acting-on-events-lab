65c5ef9585347f6c378cfa6599f01d17a50bdd98
BenedictKipkuruiLangat <benedictlangat2000@gmail.com>
2023-07-19 14:03:59 +0300

    Complted Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#000000";
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const right = left + 40; // Calculate the right edge of the dodger
+  
+    if (right < window.innerWidth) { // Check if the right edge is within the game field
+        dodger.style.left = `${left + 1}px`; // Move the dodger to the right
+    }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+  } else if (e.key === "ArrowRight") { // Add condition for right arrow key
+      moveDodgerRight();
+  }
+});
